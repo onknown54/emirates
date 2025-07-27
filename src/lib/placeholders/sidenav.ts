fcdce76edaa6dc25d534f8d5620f9a1bba14fea9
lekan <lekanm715@gmail.com>
2025-07-27 21:37:02 +0100

    added new componenets to the application

---
+++ src/lib/placeholders/sidenav.ts
@@ -0,0 +1,10 @@
+export const SidenavLinkItems = [
+  { icon: "icons/road-horizon.svg", nameTitle: "Activities" },
+  { icon: "icons/buildings.svg", nameTitle: "Hotels" },
+  { icon: "icons/airplane-tilt.svg", nameTitle: "Flights" },
+  { icon: "icons/student.svg", nameTitle: "Study" },
+  { icon: "icons/road-horizon.svg", nameTitle: "Visa" },
+  { icon: "icons/suitcase-rolling.svg", nameTitle: "Immegration" },
+  { icon: "icons/first-aid-kit.svg", nameTitle: "Medical" },
+  { icon: "icons/newspaper-clipping.svg", nameTitle: "Vacasion packages" },
+];
