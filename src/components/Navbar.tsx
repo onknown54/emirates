d6dcc950807d6c3b62147d6f2075f351f80ae18f
lekan <lekanm715@gmail.com>
2025-07-27 04:40:51 +0100

    finished up on navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { Searchbar } from "../ui/navbar";
+import { NavLeftLinks, NavRightLinks } from "../lib/placeholders/navbar";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between bg-white px-7 py-5">
+    <div className="flex justify-between bg-white px-7 py-5 text-black/50">
       <div className="flex items-center gap-3">
         <Image
           width={67}
@@ -11,27 +13,29 @@ export default function Navbar() {
           src="images/logo.svg"
           className="-mb-2 -ml-1 max-h-3/2 flex-1/5 shrink-0"
         />
-        <div className="bg-gray flex items-center justify-between overflow-hidden rounded-lg">
-          <div className="flex items-center justify-center p-3">
-            <Image
-              width={20}
-              height={20}
-              alt="logo image"
-              src="icons/search.svg"
-              className="object-contain"
-            />
-          </div>
-          <input
-            type="text"
-            name="travels"
-            id="travels"
-            placeholder="search"
-            className="bg-gray h-full w-full p-3 pl-0 text-black/50 outline-0"
-          />
-        </div>
+        <Searchbar />
       </div>
 
       {/* navbar section */}
+      <nav className="flex items-center self-center">
+        <ul className="flex list-none items-center justify-evenly gap-1">
+          {NavLeftLinks.map((itm, k) => (
+            <li
+              key={k}
+              className="hover:bg-gray inline-flex min-w-[5rem] cursor-pointer flex-col items-center justify-between gap-1.5 p-2.5 font-semibold transition-all hover:font-bold hover:text-blue-500"
+            >
+              <Image
+                src={itm.icon}
+                width={itm.width}
+                height={itm.height}
+                alt={itm.alt}
+              />
+              <span className="text-xs">{itm.navTitle}</span>
+            </li>
+          ))}
+        </ul>
+        <ul></ul>
+      </nav>
     </div>
   );
 }
