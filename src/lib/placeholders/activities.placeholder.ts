bda3ad19bd3a77682ca1abcbf4c1d1642dcd8174
lekan <lekanm715@gmail.com>
2025-08-01 01:17:57 +0100

    updates

---
+++ src/lib/placeholders/activities.placeholder.ts
@@ -0,0 +1,69 @@
+import { ActivityCardProps } from "../type-definations";
+
+export const ActivityItems: ActivityCardProps[] = [
+  {
+    rating: 4.5,
+    price: 123450,
+    dayTag: "Day 1",
+    reviewsCount: 436,
+    duration: "1 Hour",
+    imageUrl: "/images/hotel.jpg",
+    dateTime: "10:30 AM on Mar 19",
+    title: "The Museum of Modern Art",
+    included: "Admission to the Empire State Building",
+    description:
+      "Works from Van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The modern restaurant",
+  },
+  {
+    rating: 4.5,
+    price: 123450,
+    dayTag: "Day 1",
+    reviewsCount: 436,
+    duration: "1 Hour",
+    imageUrl: "/images/hotel.jpg",
+    dateTime: "10:30 AM on Mar 19",
+    title: "The Museum of Modern Art",
+    included: "Admission to the Empire State Building",
+    description:
+      "Works from Van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The modern restaurant",
+  },
+  {
+    rating: 4.5,
+    price: 123450,
+    dayTag: "Day 1",
+    reviewsCount: 436,
+    duration: "1 Hour",
+    imageUrl: "/images/hotel.jpg",
+    dateTime: "10:30 AM on Mar 19",
+    title: "The Museum of Modern Art",
+    included: "Admission to the Empire State Building",
+    description:
+      "Works from Van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The modern restaurant",
+  },
+  {
+    rating: 4.5,
+    price: 123450,
+    dayTag: "Day 1",
+    reviewsCount: 436,
+    duration: "1 Hour",
+    imageUrl: "/images/hotel.jpg",
+    dateTime: "10:30 AM on Mar 19",
+    title: "The Museum of Modern Art",
+    included: "Admission to the Empire State Building",
+    description:
+      "Works from Van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The modern restaurant",
+  },
+  {
+    rating: 4.5,
+    price: 123450,
+    dayTag: "Day 1",
+    reviewsCount: 436,
+    duration: "1 Hour",
+    imageUrl: "/images/hotel.jpg",
+    dateTime: "10:30 AM on Mar 19",
+    title: "The Museum of Modern Art",
+    included: "Admission to the Empire State Building",
+    description:
+      "Works from Van Gogh to Warhol & beyond plus a sculpture garden, 2 cafes & The modern restaurant",
+  },
+];
---
+++ src/lib/placeholders/hotels.placeholder.ts
@@ -0,0 +1,81 @@
+import { MusicalNoteIcon, WifiIcon } from "@heroicons/react/24/outline";
+import { HotelCardProps } from "../type-definations";
+
+export const HotelListing: HotelCardProps[] = [
+  {
+    price: 1834560,
+    reviewsCount: 436,
+    totalPrice: 569000,
+    checkIn: "20-04-2024",
+    checkOut: "29-04-2024",
+    roomType: "King size room",
+    imageUrl: "/images/hotel.jpg",
+    name: "Riviera Resort, Lekki",
+    nights: 10,
+    address:
+      "18, Kenneth Agbakuru Street, Off Access Bank Admiralty Way, Lekki Phase1",
+    facilities: [
+      { label: "Pool", icon: WifiIcon },
+      { label: "Bar", icon: MusicalNoteIcon },
+      { label: "Wi‑Fi", icon: WifiIcon },
+      { label: "Live music", icon: MusicalNoteIcon },
+    ],
+  },
+  {
+    price: 1234560,
+    reviewsCount: 436,
+    totalPrice: 569000,
+    checkIn: "20-04-2024",
+    checkOut: "29-04-2024",
+    roomType: "King size room",
+    imageUrl: "/images/hotel.jpg",
+    name: "Riviera Resort, Lekki",
+    nights: 10,
+    address:
+      "18, Kenneth Agbakuru Street, Off Access Bank Admiralty Way, Lekki Phase1",
+    facilities: [
+      { label: "Pool", icon: WifiIcon },
+      { label: "Bar", icon: MusicalNoteIcon },
+      { label: "Wi‑Fi", icon: WifiIcon },
+      { label: "Live music", icon: MusicalNoteIcon },
+    ],
+  },
+  {
+    price: 1234560,
+    reviewsCount: 436,
+    totalPrice: 569000,
+    checkIn: "20-04-2024",
+    checkOut: "29-04-2024",
+    roomType: "King size room",
+    imageUrl: "/images/hotel.jpg",
+    name: "Riviera Resort, Lekki",
+    nights: 10,
+    address:
+      "18, Kenneth Agbakuru Street, Off Access Bank Admiralty Way, Lekki Phase1",
+    facilities: [
+      { label: "Pool", icon: WifiIcon },
+      { label: "Bar", icon: MusicalNoteIcon },
+      { label: "Wi‑Fi", icon: WifiIcon },
+      { label: "Live music", icon: MusicalNoteIcon },
+    ],
+  },
+  {
+    price: 1234560,
+    reviewsCount: 436,
+    totalPrice: 569000,
+    checkIn: "20-04-2024",
+    checkOut: "29-04-2024",
+    roomType: "King size room",
+    imageUrl: "/images/hotel.jpg",
+    name: "Riviera Resort, Lekki",
+    nights: 10,
+    address:
+      "18, Kenneth Agbakuru Street, Off Access Bank Admiralty Way, Lekki Phase1",
+    facilities: [
+      { label: "Pool", icon: WifiIcon },
+      { label: "Bar", icon: MusicalNoteIcon },
+      { label: "Wi‑Fi", icon: WifiIcon },
+      { label: "Live music", icon: MusicalNoteIcon },
+    ],
+  },
+];
